d025cb0bdaf9dd2acaa6b988462d3058d207e017
Kawsar <imkawsar007@gmail.com>
2021-01-11 21:34:36 +0600

    Implement Stars and Forks Chart

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -2,5 +2,6 @@ import Column3D from './Column3D';
 import Doughnut2d from './Doughnut2d';
 import ExampleChart from './ExampleChart';
 import Pie3D from './Pie3D';
+import Bar3d from './Bar3d';
 
-export { Column3D, ExampleChart, Pie3D, Doughnut2d };
+export { Column3D, ExampleChart, Pie3D, Doughnut2d, Bar3d };
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,50 +1,77 @@
 import React from "react";
 import styled from "styled-components";
 import { GithubContext } from "../context/context";
-import { Doughnut2d, ExampleChart, Pie3D } from "./Charts";
+import { Column3D, Doughnut2d, ExampleChart, Pie3D, Bar3d} from "./Charts";
 //import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
 
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
     if (!language) return total;
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
     return total;
   }, {});
-  languages = Object.values(languages).sort((a, b) => {
-      return b.value - a.value;
-  }).slice(0, 5);
   console.log(languages);
-  const chartData = [
-    {
-      label: "HTML",
-      value: "15",
-    },
-    {
-      label: "CSS",
-      value: "23",
-    },
-    {
-      label: "Javascript",
-      value: "160",
-    },
-  ];
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  // most stars per language
+
+  const mostPopular = Object.values(languages).sort((a, b) => {
+    return b.stars - a.stars;
+  }).map((item) => {
+    return { ...item, value: item.stars };
+  }).slice(0,5);
+
+  // stars. forks
+  let { stars, forks } = repos.reduce((total, item) => {
+    const { stargazers_count, name, forks } = item;
+    total.stars[stargazers_count] = { label: name, value: stargazers_count };
+    total.forks[forks] = { label: name, value: forks };
+    return total;
+  }, {
+    stars: {},
+    forks: {}
+  });
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
+  // const chartData = [
+  //   {
+  //     label: "HTML",
+  //     value: "15",
+  //   },
+  //   {
+  //     label: "CSS",
+  //     value: "23",
+  //   },
+  //   {
+  //     label: "Javascript",
+  //     value: "160",
+  //   },
+  // ];
   return (
     <section className="section">
       <Wrapper className="section-center">
         {/* <ExampleChart data={chartData}/> */}
-        <Pie3D data={languages} />
-        <div></div>
-        <Doughnut2d data={chartData}/>
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars}/>
+        <Doughnut2d data={mostPopular} />
+        <Bar3d data={forks}/>
       </Wrapper>
     </section>
   );
